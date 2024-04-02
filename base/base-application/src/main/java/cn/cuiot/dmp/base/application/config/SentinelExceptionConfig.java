6c66ae8a9819f9c349af7d240a68d8bbe97cf19a
yongchongwu <yongchongwu@126.com>
2024-04-02 21:12:51 +0800

    feat(限流): 解决sentinel返回中文乱码
    解决sentinel返回中文乱码

--- base/base-application/src/main/java/cn/cuiot/dmp/base/application/config/SentinelExceptionConfig.java
+++ base/base-application/src/main/java/cn/cuiot/dmp/base/application/config/SentinelExceptionConfig.java
@@ -6,6 +6,7 @@ import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHan
 import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.io.CharTypes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,7 @@ public class SentinelExceptionConfig implements BlockExceptionHandler {
 
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws Exception {
+        httpServletResponse.setCharacterEncoding("UTF-8");
         httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
         httpServletResponse.getWriter().write(JSON.toJSONString(new IdmResDTO<>(ResultCode.SERVER_BUSY)));
     }
