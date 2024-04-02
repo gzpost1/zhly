14baa3aa39f2f212f9a7bfc82bbc303928833ae2
yongchongwu <yongchongwu@126.com>
2024-04-02 17:50:14 +0800

    feat(限流): 增加sentinel限流组件支持
    增加sentinel限流组件支持

--- base/base-application/src/main/java/cn/cuiot/dmp/base/application/config/SentinelExceptionConfig.java
+++ base/base-application/src/main/java/cn/cuiot/dmp/base/application/config/SentinelExceptionConfig.java
@@ -3,10 +3,12 @@ package cn.cuiot.dmp.base.application.config;
 import cn.cuiot.dmp.common.constant.IdmResDTO;
 import cn.cuiot.dmp.common.constant.ResultCode;
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 
@@ -15,6 +17,11 @@ import org.springframework.http.MediaType;
  */
 @Configuration
 public class SentinelExceptionConfig implements BlockExceptionHandler {
+
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
 
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws Exception {
