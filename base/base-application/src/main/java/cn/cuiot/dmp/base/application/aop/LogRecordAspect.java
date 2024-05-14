d42cfc79c80045c758d3953cdaa774487edc0f5b
libo <623178903@qq.com>
2024-05-14 15:35:01 +0800

    feat(流程配置): 流程配置解决精度问题

--- base/base-application/src/main/java/cn/cuiot/dmp/base/application/aop/LogRecordAspect.java
+++ base/base-application/src/main/java/cn/cuiot/dmp/base/application/aop/LogRecordAspect.java
@@ -10,14 +10,10 @@ import cn.cuiot.dmp.common.constant.IdmResDTO;
 import cn.cuiot.dmp.common.enums.LogLevelEnum;
 import cn.cuiot.dmp.common.enums.StatusCodeEnum;
 import cn.cuiot.dmp.common.log.dto.OperateLogDto;
-import cn.cuiot.dmp.common.log.intf.ResourceParam;
-import cn.cuiot.dmp.domain.types.LoginInfoHolder;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.RequestFacade;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -26,22 +22,17 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -149,18 +140,6 @@ public class LogRecordAspect extends BaseController {
         } catch (Exception e) {
             log.error("LogRecordAspect joinPoint.proceed error", e);
 
-            // 获取操作对象
-//            String operationTarget = this.getOperationTarget(joinPoint);
-//            if (StringUtils.isNotBlank(operationTarget)) {
-//                operateLogDto.setOperationTarget(operationTarget);
-//            }
-            operateLogDto.setOperationTarget(LoginInfoHolder.getCurrentUserId().toString());
-            operateLogDto.setOperationTargetInfo(LoginInfoHolder.getCurrentUserId().toString());
-
-//            String operationTargetInfo = getOperationTargetInfo(joinPoint);
-//            if (StringUtils.isNotBlank(operationTargetInfo)) {
-//                operateLogDto.setOperationTargetInfo(operationTargetInfo);
-//            }
 
             operateLogDto.setLogLevel(LogLevelEnum.ERROR.getCode());
             operateLogDto.setStatusCode(StatusCodeEnum.FAILED.getCode());
@@ -172,58 +151,6 @@ public class LogRecordAspect extends BaseController {
         }
 
         return obj;
-    }
-
-    /**
-     * 获取操作对象
-     */
-    private String getOperationTarget(ProceedingJoinPoint joinPoint) throws Throwable {
-        String operationTarget = null;
-
-        try {
-            Object[] args = joinPoint.getArgs();
-            Method method = ResourceParam.class.getDeclaredMethod("getOperationTarget");
-            if (null != args && null != method) {
-                Object result = method.invoke(args[0]);
-                if (null != result) {
-                    String[] operationTargetArray = (String[]) result;
-                    if (ArrayUtils.isNotEmpty(operationTargetArray)) {
-                        StringBuilder sb = new StringBuilder();
-                        for (int i = 0; i < operationTargetArray.length; i++) {
-                            sb.append(operationTargetArray[i]).append(",");
-                        }
-                        sb.deleteCharAt(sb.length() - 1);
-                        operationTarget = sb.toString();
-                    }
-                }
-            }
-        } catch (Exception e) {
-            log.error("获取操作对象错误，{}", e.getMessage());
-        }
-
-        return operationTarget;
-    }
-
-    /**
-     * 获取操作对象信息
-     */
-    private String getOperationTargetInfo(ProceedingJoinPoint joinPoint) throws Throwable {
-        String operationTargetInfo = null;
-
-        try {
-            Object[] args = joinPoint.getArgs();
-            Method method = ResourceParam.class.getDeclaredMethod("getOperationTargetInfo");
-            if (null != args && null != method) {
-                Object result = method.invoke(args[0]);
-                if (null != result) {
-                    operationTargetInfo = (String) result;
-                }
-            }
-        } catch (Exception e) {
-            log.error("获取操作对象信息错误，{}", e.getMessage());
-        }
-
-        return operationTargetInfo;
     }
 
     /**
@@ -262,9 +189,6 @@ public class LogRecordAspect extends BaseController {
                               Object obj) throws Throwable {
         //方法执行后
         try {
-            // 获取操作对象
-            operateLogDto.setOperationTarget(LoginInfoHolder.getCurrentUserId().toString());
-            operateLogDto.setOperationTargetInfo(LoginInfoHolder.getCurrentUserId().toString());
 
             if (obj instanceof IdmResDTO) {
                 IdmResDTO respDto = (IdmResDTO) obj;
@@ -297,29 +221,6 @@ public class LogRecordAspect extends BaseController {
             map.put(key, parameterMap.get(key)[0]);
         }
         return map;
-    }
-
-    /**
-     * 获取request body中的参数
-     *
-     * @param request 请求
-     */
-    public String getParameterByRequest(HttpServletRequest request) throws IOException {
-        List<String> lines = null;
-        try {
-            lines = IOUtils.readLines(request.getInputStream());
-            log.info("获取request body中的参数: " + JSON.toJSONString(lines));
-        } catch (IOException e) {
-            log.info("获取request body中的参数错误：" + e.getMessage());
-            throw e;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        if (!CollectionUtils.isEmpty(lines)) {
-            lines.forEach(sb::append);
-        }
-
-        return sb.toString();
     }
 
 }
