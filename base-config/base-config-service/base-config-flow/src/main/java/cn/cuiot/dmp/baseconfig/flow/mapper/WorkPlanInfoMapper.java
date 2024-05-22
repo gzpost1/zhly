a588e1600c93b898921b798039c5869d4d6cc269
pengjian <18275188670@163.com>
2024-05-22 18:52:23 +0800

    202405221738bug处理

--- base-config/base-config-service/base-config-flow/src/main/java/cn/cuiot/dmp/baseconfig/flow/mapper/WorkPlanInfoMapper.java
+++ base-config/base-config-service/base-config-flow/src/main/java/cn/cuiot/dmp/baseconfig/flow/mapper/WorkPlanInfoMapper.java
@@ -1,8 +1,11 @@
 package cn.cuiot.dmp.baseconfig.flow.mapper;
 
 
+import cn.cuiot.dmp.baseconfig.flow.dto.QueryWorkPlanInfoDto;
 import cn.cuiot.dmp.baseconfig.flow.entity.WorkPlanInfoEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
@@ -14,4 +17,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface WorkPlanInfoMapper extends BaseMapper<WorkPlanInfoEntity> {
 
+    Page<WorkPlanInfoEntity> queryWordPlanInfo(Page<WorkPlanInfoEntity> page, @Param("query") QueryWorkPlanInfoDto dto);
 }
