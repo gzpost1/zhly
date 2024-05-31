75784f240ee6791369f6e615ab36b94252d92a25
caorui <caorui@yunjingtech.cn>
2024-05-31 10:44:22 +0800

    fix(V1.1)：合并自定义配置和常用选项

--- common/src/main/java/cn/cuiot/dmp/common/constant/CustomConfigConstant.java
+++ common/src/main/java/cn/cuiot/dmp/common/constant/CustomConfigConstant.java
@@ -13,11 +13,11 @@ public class CustomConfigConstant {
     /**
      * 自定义配置相关
      */
-    public static final List<String> HOUSES_ARCHIVES_INIT = Lists.newArrayList("房屋户型", "朝向", "物业业态",
-            "状态", "用途", "经营性质", "资源类型", "产权属性", "停车区域", "基础服务");
-    public static final List<String> ROOM_ARCHIVES_INIT = Lists.newArrayList("空间分类", "专业用途", "经营性质",
-            "产权属性", "资源类型", "物业服务档次", "定位方式");
-    public static final List<String> DEVICE_ARCHIVES_INIT = Lists.newArrayList("设备类别", "设备状态", "物业服务档次");
-    public static final List<String> PARKING_ARCHIVES_INIT = Lists.newArrayList("所属区域", "使用情况", "车位类型");
+    public static final List<String> HOUSES_ARCHIVES_INIT = Lists.newArrayList("房屋户型", "房屋朝向", "房屋物业业态",
+            "房屋状态", "房屋用途", "房屋经营性质", "房屋资源类型", "房屋产权属性", "房屋停车区域", "房屋基础服务");
+    public static final List<String> ROOM_ARCHIVES_INIT = Lists.newArrayList("空间分类", "空间专业用途", "空间经营性质",
+            "空间产权属性", "空间资源类型", "空间物业服务档次", "空间定位方式");
+    public static final List<String> DEVICE_ARCHIVES_INIT = Lists.newArrayList("设备类别", "设备状态", "设备物业服务档次");
+    public static final List<String> PARKING_ARCHIVES_INIT = Lists.newArrayList("车位所属区域", "车位使用情况", "车位类型");
 
 }
