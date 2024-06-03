91a1bcbaee481981edd9c0817dc349834a3ab7f6
yongchongwu <yongchongwu@126.com>
2024-06-03 16:32:31 +0800

    feat(APP): 优化appp认证登录
    优化appp认证登录

--- app/app-provider/src/main/java/cn/cuiot/dmp/app/controller/AuthController.java
+++ app/app-provider/src/main/java/cn/cuiot/dmp/app/controller/AuthController.java
@@ -46,7 +46,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/app/auth")
 public class AuthController {
 
     @Autowired
