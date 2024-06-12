e63923b1b1e905763914adab4f1d5b98507c9e6b
yongchongwu <yongchongwu@126.com>
2024-06-12 17:39:35 +0800

    feat(档案中心): 增加客户管理
    增加客户管理

---
+++ archive/src/main/java/cn/cuiot/dmp/archive/infrastructure/vo/CustomerMemberVo.java
@@ -0,0 +1,14 @@
+package cn.cuiot.dmp.archive.infrastructure.vo;
+
+import cn.cuiot.dmp.archive.infrastructure.entity.CustomerMemberEntity;
+import lombok.Data;
+
+/**
+ * 客户成员信息
+ * @author: wuyongchong
+ * @date: 2024/6/12 14:57
+ */
+@Data
+public class CustomerMemberVo extends CustomerMemberEntity {
+
+}
