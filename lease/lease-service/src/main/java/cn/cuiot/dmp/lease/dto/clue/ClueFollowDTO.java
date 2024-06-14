503ea0ab7523f7517873a60ee4bd8105be8f51c5
caorui <caorui@yunjingtech.cn>
2024-06-14 15:15:00 +0800

    feat(V2.0)：线索管理

--- lease/lease-service/src/main/java/cn/cuiot/dmp/lease/dto/clue/ClueFollowDTO.java
+++ lease/lease-service/src/main/java/cn/cuiot/dmp/lease/dto/clue/ClueFollowDTO.java
@@ -1,5 +1,6 @@
 package cn.cuiot.dmp.lease.dto.clue;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
@@ -47,7 +48,7 @@ public class ClueFollowDTO implements Serializable {
     /**
      * 线索表单配置数据
      */
-    private String formData;
+    private JSONObject formData;
 
     /**
      * 当前线索表单配置快照
