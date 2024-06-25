2fd67b5f37619eede57833017bb41c923abd53e0
pengjian <18275188670@163.com>
2024-06-25 17:12:48 +0800

    2.0新增子节点类型数量查询

--- base-config/base-config-service/base-config-flow/src/main/java/cn/cuiot/dmp/baseconfig/flow/service/WorkInfoService.java
+++ base-config/base-config-service/base-config-flow/src/main/java/cn/cuiot/dmp/baseconfig/flow/service/WorkInfoService.java
@@ -1848,9 +1848,14 @@ public class WorkInfoService extends ServiceImpl<WorkInfoMapper, WorkInfoEntity>
 
         LambdaQueryWrapper<CommitProcessEntity> processLw = new LambdaQueryWrapper<>();
         processLw.eq(Objects.nonNull(dto.getProcInstId()),CommitProcessEntity::getProcInstId,dto.getProcInstId())
-                .eq(Objects.nonNull(dto.getNodeId()),CommitProcessEntity::getNodeId,dto.getNodeId())
-                .eq(Objects.nonNull(dto.getUserId()),CommitProcessEntity::getUserId,dto.getUserId())
-                .eq(Objects.nonNull(dto.getBusinessTypeId()),CommitProcessEntity::getBusinessTypeId,dto.getBusinessTypeId());
+                .eq(Objects.nonNull(dto.getNodeId()),CommitProcessEntity::getNodeId,dto.getNodeId());
+                if(Objects.isNull(dto.getUserId())){
+                    processLw.eq(CommitProcessEntity::getUserId,LoginInfoHolder.getCurrentUserId());
+                }else{
+                    processLw.eq(CommitProcessEntity::getUserId,dto.getUserId());
+                }
+        processLw.eq(Objects.nonNull(dto.getBusinessTypeId()),CommitProcessEntity::getBusinessTypeId,dto.getBusinessTypeId())
+                .orderByDesc(CommitProcessEntity::getCreateTime);
         List<CommitProcessEntity> processList = commitProcessService.list(processLw);
 
         ProcessResultDto resultDto = new ProcessResultDto();
