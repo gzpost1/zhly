d43a723f4ae364820b7c178f0262f09603ca553c
zhangcong <39365090+eeead@users.noreply.github.com>
2024-06-28 16:39:15 +0800

    fix(2.1v):提供常用选项设置表外部调用接口

--- domain/domain-system/src/main/java/cn/cuiot/dmp/system/domain/repository/CommonOptionSettingRepository.java
+++ domain/domain-system/src/main/java/cn/cuiot/dmp/system/domain/repository/CommonOptionSettingRepository.java
@@ -25,4 +25,8 @@ public interface CommonOptionSettingRepository {
      */
     void batchDeleteCommonOptionSettings(List<Long> commonOptionIdList);
 
+    /**
+     * 根据常用选项批量查询常用选项分类
+     */
+    List<CommonOptionSetting> batchQueryCommonOptionSettingsByIds(List<Long> ids);
 }
--- system/src/main/java/cn/cuiot/dmp/system/api/controller/ApiController.java
+++ system/src/main/java/cn/cuiot/dmp/system/api/controller/ApiController.java
@@ -10,11 +10,14 @@ import cn.cuiot.dmp.base.infrastructure.dto.MenuDTO;
 import cn.cuiot.dmp.common.constant.IdmResDTO;
 import cn.cuiot.dmp.common.constant.ResultCode;
 import cn.cuiot.dmp.common.exception.BusinessException;
+import cn.cuiot.dmp.common.utils.AssertUtil;
 import cn.cuiot.dmp.system.application.param.assembler.DepartmentConverter;
 import cn.cuiot.dmp.system.application.param.assembler.MenuConverter;
 import cn.cuiot.dmp.system.application.param.dto.FormConfigDTO;
 import cn.cuiot.dmp.system.application.param.vo.FormConfigVO;
 import cn.cuiot.dmp.system.application.service.*;
+import cn.cuiot.dmp.system.domain.aggregate.CommonOptionSetting;
+import cn.cuiot.dmp.system.domain.repository.CommonOptionSettingRepository;
 import cn.cuiot.dmp.system.infrastructure.entity.DepartmentEntity;
 import cn.cuiot.dmp.system.infrastructure.entity.MenuEntity;
 
@@ -74,6 +77,8 @@ public class ApiController {
 
     @Autowired
     private AuditConfigTypeService auditConfigTypeService;
+    @Autowired
+    private CommonOptionSettingRepository commonOptionSettingRepository;
 
     /**
      * 查询角色
@@ -260,4 +265,13 @@ public class ApiController {
         return IdmResDTO.success(auditConfigTypeRspDTOList);
     }
 
+    /**
+     * 批量查询表单配置-常用选项设置数据
+     */
+    @PostMapping(value = "/batchQueryCommonOptionSetting", produces = MediaType.APPLICATION_JSON_VALUE)
+    public IdmResDTO<List<CommonOptionSetting>> batchQueryCommonOptionSetting(@RequestBody @Valid CommonOptionSettingReqDTO dto) {
+        AssertUtil.isFalse(CollectionUtils.isEmpty(dto.getIdList()),"常用选项设置ID列表不能为空");
+        List<CommonOptionSetting> optionSettings = commonOptionSettingRepository.batchQueryCommonOptionSettingsByIds(dto.getIdList());
+        return IdmResDTO.success(optionSettings);
+    }
 }
