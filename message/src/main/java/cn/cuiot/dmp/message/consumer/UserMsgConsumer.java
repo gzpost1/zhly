e41d183501f90b3374e680bd42f854acb7fa4aa9
hantingyao <123@qq.com>
2024-07-17 16:00:19 +0800

    v2.0: 消息删除同一条消息数据只接受一次限制

--- message/src/main/java/cn/cuiot/dmp/message/consumer/UserMsgConsumer.java
+++ message/src/main/java/cn/cuiot/dmp/message/consumer/UserMsgConsumer.java
@@ -40,7 +40,6 @@ public class UserMsgConsumer {
             UserMessageEntity userMessage = UserMessageConvert.INSTANCE.concert(userMessageAcceptDto.getSysMsgDto());
             userMessage.init();
             List<UserMessageEntity> userMessageEntities = dealMsgByType(userMessage, userMessageAcceptDto.getSysMsgDto());
-            System.out.println(JsonUtil.writeValueAsString(userMessageEntities));
             userMessageService.saveBatch(userMessageEntities);
         }
 
@@ -48,14 +47,12 @@ public class UserMsgConsumer {
 
     //TODO
     private List<UserMessageEntity> dealMsgByType(UserMessageEntity userMessage, SysMsgDto sysMsgDto) {
-        List<Long> accepters = userMessageService.queryByDataId(sysMsgDto.getDataId());
+
         List<UserMessageEntity> userMessageEntities = new ArrayList<>();
         sysMsgDto.getAcceptors().forEach(acceptor -> {
-            if (!accepters.contains(acceptor)) {
-                UserMessageEntity userMessageEntity = BeanUtil.copyProperties(userMessage, UserMessageEntity.class);
-                userMessageEntity.setAccepter(acceptor);
-                userMessageEntities.add(userMessageEntity);
-            }
+            UserMessageEntity userMessageEntity = BeanUtil.copyProperties(userMessage, UserMessageEntity.class);
+            userMessageEntity.setAccepter(acceptor);
+            userMessageEntities.add(userMessageEntity);
         });
         return userMessageEntities;
     }
