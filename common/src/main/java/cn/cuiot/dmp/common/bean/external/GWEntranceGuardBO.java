ae6b0c17d3af00cd68a0a6818e138567972ab601
zhangcong <39365090+eeead@users.noreply.github.com>
2024-09-04 09:47:15 +0800

    feat(3.2v):新增对接类型

---
+++ common/src/main/java/cn/cuiot/dmp/common/bean/external/GWEntranceGuardBO.java
@@ -0,0 +1,18 @@
+package cn.cuiot.dmp.common.bean.external;
+
+import lombok.Data;
+
+/**
+ * 门禁（格物）-对接系统参数
+ *
+ * @Author: zc
+ * @Date: 2024-09-04
+ */
+@Data
+public class GWEntranceGuardBO {
+
+    /**
+     * productKey
+     */
+    private String productKey;
+}
