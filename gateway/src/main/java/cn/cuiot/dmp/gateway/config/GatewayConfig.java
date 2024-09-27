87f842faac412e8c60c22ae6371a882f3177b4d4
yongchongwu <yongchongwu@126.com>
2024-09-27 09:42:16 +0800

    feat(漏洞修复-网关跨域配置): 网关跨域限制配置
    网关跨域限制配置

--- gateway/src/main/java/cn/cuiot/dmp/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/cn/cuiot/dmp/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package cn.cuiot.dmp.gateway.config;
 
+import javax.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,24 +9,27 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 /**
  * 网关相关配置
+ *
  * @author: wuyongchong
  * @date: 2024/2/19 13:29
  */
 @Configuration
 public class GatewayConfig {
 
+    @Resource
+    private WebCorsProperties webCorsProperties;
+
     /**
      * 跨域配置
-     * @return
      */
     @Bean
     public CorsWebFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(webCorsProperties.getAllowCredentials());
+        config.setAllowedHeaders(webCorsProperties.getAllowedHeaders());
+        config.setAllowedMethods(webCorsProperties.getAllowedMethods());
+        config.setAllowedOriginPatterns(webCorsProperties.getAllowedOrigins());
         source.registerCorsConfiguration("/**", config);
         return new CorsWebFilter(source);
     }
