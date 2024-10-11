bd2e06dec8b38e77f9aeb12ecaa3d2b1608ba39a
yongchongwu <yongchongwu@126.com>
2024-10-11 16:18:17 +0800

    feat(V3.7-海康门禁): V3.7-海康门禁定时任务测试
    V3.7-海康门禁定时任务测试

---
+++ externalapi/externalapi-provider/src/test/java/cn/cuiot/dmp/externalapi/HaikangAcsReaderDataSyncTaskTest.java
@@ -0,0 +1,76 @@
+package cn.cuiot.dmp.externalapi;
+
+import cn.cuiot.dmp.base.infrastructure.dto.rsp.PlatfromInfoRespDTO;
+import cn.cuiot.dmp.externalapi.provider.ExternalapiApplication;
+import cn.cuiot.dmp.externalapi.service.sync.hik.HaikangAcsReaderDataSyncService;
+import cn.cuiot.dmp.externalapi.service.sync.hik.HaikangPlatfromInfoCallable;
+import cn.cuiot.dmp.externalapi.service.sync.hik.HaikangPlatfromInfoCallableService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: wuyongchong
+ * @date: 2024/10/11 11:47
+ */
+@Slf4j
+@SpringBootTest(classes = ExternalapiApplication.class)
+@RunWith(SpringRunner.class)
+public class HaikangAcsReaderDataSyncTaskTest {
+
+    @Autowired
+    private HaikangAcsReaderDataSyncService haikangAcsReaderDataSyncService;
+
+    @Autowired
+    private HaikangPlatfromInfoCallableService haikangPlatfromInfoCallableService;
+
+    @Test
+    public void fullDataSyncTest() {
+        haikangPlatfromInfoCallableService.resolvePlatfromInfos(1,
+                new HaikangPlatfromInfoCallable() {
+                    @Override
+                    public void process(List<PlatfromInfoRespDTO> platfromInfoList) {
+                        if (CollectionUtils.isNotEmpty(platfromInfoList)) {
+                            Long companyId = platfromInfoList.get(0).getCompanyId();
+                            haikangAcsReaderDataSyncService.haikangAcsReaderFullDataSync(companyId);
+                        }
+                    }
+                });
+
+    }
+
+    @Test
+    public void incrementDataSyncTest() {
+        haikangPlatfromInfoCallableService.resolvePlatfromInfos(1,
+                new HaikangPlatfromInfoCallable() {
+                    @Override
+                    public void process(List<PlatfromInfoRespDTO> platfromInfoList) {
+                        if (CollectionUtils.isNotEmpty(platfromInfoList)) {
+                            Long companyId = platfromInfoList.get(0).getCompanyId();
+                            haikangAcsReaderDataSyncService.hikAcsReaderIncrementDataSync(
+                                    companyId);
+                        }
+                    }
+                });
+
+    }
+
+    @Test
+    public void statusSyncTest() {
+        haikangPlatfromInfoCallableService.resolvePlatfromInfos(1,
+                new HaikangPlatfromInfoCallable() {
+                    @Override
+                    public void process(List<PlatfromInfoRespDTO> platfromInfoList) {
+                        if (CollectionUtils.isNotEmpty(platfromInfoList)) {
+                            Long companyId = platfromInfoList.get(0).getCompanyId();
+                            haikangAcsReaderDataSyncService.hikAcsReaderOnlineStatusSync(companyId);
+                        }
+                    }
+                });
+    }
+}
