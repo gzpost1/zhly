cb77223f16a0168065a60dbb7862bc995d4fa6fe
libo <623178903@qq.com>
2024-10-16 15:10:18 +0800

    feat(3.5): 预缴支付配置

---
+++ lease/lease-service/src/main/java/cn/cuiot/dmp/pay/service/service/mapper/TbPrePayAutoConfigMapper.java
@@ -0,0 +1,7 @@
+package cn.cuiot.dmp.pay.service.service.mapper;
+
+import cn.cuiot.dmp.pay.service.service.entity.TbPrePayAutoConfig;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface TbPrePayAutoConfigMapper extends BaseMapper<TbPrePayAutoConfig> {
+}
---
+++ lease/lease-service/src/main/java/cn/cuiot/dmp/pay/service/service/service/TbPrePayAutoConfigService.java
@@ -0,0 +1,12 @@
+package cn.cuiot.dmp.pay.service.service.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import cn.cuiot.dmp.pay.service.service.mapper.TbPrePayAutoConfigMapper;
+import cn.cuiot.dmp.pay.service.service.entity.TbPrePayAutoConfig;
+@Service
+public class TbPrePayAutoConfigService extends ServiceImpl<TbPrePayAutoConfigMapper, TbPrePayAutoConfig> {
+
+}
