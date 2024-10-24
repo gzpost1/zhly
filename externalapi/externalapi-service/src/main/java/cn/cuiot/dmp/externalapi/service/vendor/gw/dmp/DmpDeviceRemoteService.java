e5f4cc37b6000bba0e2cf60156f99255e2ccfef2
zhangcong <39365090+eeead@users.noreply.github.com>
2024-10-24 14:51:08 +0800

    fix(3.9v)：修改给格物平台对接api

--- externalapi/externalapi-service/src/main/java/cn/cuiot/dmp/externalapi/service/vendor/gw/dmp/DmpDeviceRemoteService.java
+++ externalapi/externalapi-service/src/main/java/cn/cuiot/dmp/externalapi/service/vendor/gw/dmp/DmpDeviceRemoteService.java
@@ -96,7 +96,7 @@ public class DmpDeviceRemoteService {
     /**
      * 批量查询设备状态
      */
-    public BaseDmpResp<DmpDeviceResp> batchGetStatus(DmpDeviceBatchCreateReq req, GWCurrencyBO bo) {
+    public BaseDmpResp<DmpDeviceResp> batchGetStatus(DmpDeviceBatchStatusReq req, GWCurrencyBO bo) {
         String gateway = "api/batchGetStatus/V1/1Main/vV1.1";
         return dmpApiService.postRequest(gateway, req, bo, new TypeReference<BaseDmpResp<DmpDeviceResp>>() {
         });
