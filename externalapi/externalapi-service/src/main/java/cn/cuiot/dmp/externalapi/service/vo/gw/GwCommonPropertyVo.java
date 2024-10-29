e64d4744b3a5bb8f929ec5bd99cc2c4347655882
zhangcong <39365090+eeead@users.noreply.github.com>
2024-10-29 14:18:03 +0800

    fix(3.9v)：修改

--- externalapi/externalapi-service/src/main/java/cn/cuiot/dmp/externalapi/service/vo/gw/GwCommonPropertyVo.java
+++ externalapi/externalapi-service/src/main/java/cn/cuiot/dmp/externalapi/service/vo/gw/GwCommonPropertyVo.java
@@ -2,11 +2,13 @@ package cn.cuiot.dmp.externalapi.service.vo.gw;
 
 import cn.cuiot.dmp.externalapi.service.vendor.gw.bean.resp.DmpDevicePropertyResp;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author xulei
  * @create 2024-10-29 10:18
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class GwCommonPropertyVo extends DmpDevicePropertyResp {
 
@@ -14,4 +16,9 @@ public class GwCommonPropertyVo extends DmpDevicePropertyResp {
      * key值名称
      */
     private String keyName;
+
+    public GwCommonPropertyVo(String key, String keyName) {
+        super(key);
+        this.keyName = keyName;
+    }
 }
