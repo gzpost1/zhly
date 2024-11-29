f0b528ec753d4d4a5832ce6692ad94f3dc498c1f
mujun <mujun@yunjingtech.cn>
2024-11-29 16:12:40 +0800

    feat(v3.10.3):新增楼盘类型

--- sql/v3.10.3.sql
+++ sql/v3.10.3.sql
@@ -1,3 +1,5 @@
 alter table tb_building_archives
     add type varchar(50) null comment '楼盘类型';
 
+INSERT INTO db_smart_park.system_option_type (id, name, system_option_type, deleted_flag) VALUES (16, '楼盘类型', 16, 0);
+
